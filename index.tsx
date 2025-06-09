bb9da2da18f6eaa529bd729cf232b588c5aebb4f
Surge <surgedevs@gmail.com>
2025-06-09 02:20:07 +0200

    feat: finish csp logic

--- index.tsx
+++ index.tsx
@@ -8,9 +8,10 @@ import "./style.css";
 
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
+import { closeModal, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalRoot, openModal } from "@utils/modal";
 import definePlugin, { OptionType, PluginNative } from "@utils/types";
 import { findComponentByCodeLazy } from "@webpack";
-import { Button, Select, TextInput, useEffect, useState } from "@webpack/common";
+import { Button, Select, Text, TextInput, useEffect, useRef, useState } from "@webpack/common";
 
 const Native = VencordNative.pluginHelpers.CSPManager as PluginNative<typeof import("./native")>;
 
@@ -27,92 +28,177 @@ const settings = definePluginSettings({
         type: OptionType.COMPONENT,
         component: SettingsBlock
     },
-    cspRules: {
-        type: OptionType.CUSTOM,
-        default: {} as PolicyMap
-    }
 });
+
+type CSPolicy = { id: string; domain: string; policy: string[]; };
 
 function SettingsBlock() {
     const AddButton = findComponentByCodeLazy('.Text,{color:"interactive-active",variant:"text-md/semibold",');
-    
-    const [cspRules, setCspRules] = useState(settings.store.cspRules);
+    const [csPolicies, setCsPolicies] = useState([] as CSPolicy[]);
+    const containerRef = useRef(null as HTMLDivElement | null);
 
     useEffect(() => {
         (async () => {
-            if (Object.entries(settings.store.cspRules).length === 0) {
-                const defaultPolicies = await Native.getCurrentCSPPolicies();
+            const currentPolicies = await Native.getCurrentCSPPolicies();
+            const policies = Object.entries(currentPolicies).map(p => ({
+                id: Math.random().toString(36).substring(2, 10),
+                domain: p[0],
+                policy: p[1]
+            }));
 
-                console.log("defaultPolicies", defaultPolicies);
-
-                setCspRules(defaultPolicies);
-                settings.store.cspRules = defaultPolicies;
-            } else {
-                setCspRules(settings.store.cspRules);
-            }
+            setCsPolicies(policies);
         })();
     }, []);
 
-    return <div className="cspmanager-container">
-        {Object.entries(cspRules).map(([key, value]) => (() => {
-            const [domain, setDomain] = useState(key);
-            const [policy, setPolicy] = useState(value.join(" "));
+    const onDelete = (id: string) => {
+        // kinda disgusting but having to scroll back to the bottom is more so disgusting
+        const scroller = containerRef.current?.parentElement?.parentElement?.parentElement;
+        const currentScroll = scroller?.scrollTop;
+        setCsPolicies(p => p.filter(p => p.id !== id));
 
-            const onChangeDomain = (newDomain: string) => {
-                const newRuleSet = { ...cspRules };
-                delete newRuleSet[domain];
-                newRuleSet[newDomain] = policy.split(" ");
+        setTimeout(() => {
+            console.log(scroller);
+            if (scroller)
+                scroller.scrollTop = currentScroll || 0;
+        }, 0);
+    };
 
-                setDomain(newDomain);
-                Native.updateCSPPolicies(newRuleSet);
-            };
+    const onAdd = () => {
+        setCsPolicies([
+            ...csPolicies, {
+                id: Math.random().toString(36).substring(2, 10),
+                domain: "",
+                policy: ConnectSrc
+            }
+        ]);
+    };
 
-            const onChangePolicy = newPolicy => {
-                const newRuleSet = { ...cspRules };
-                newRuleSet[domain] = newPolicy.value;
+    const onUpdate = (id: string, domain: string, policy: string[]) => {
+        setCsPolicies(p => p.map(p => p.id === id ? { ...p, domain, policy } : { ...p }));
+    };
 
-                setPolicy(newPolicy.value.join(" "));
-                Native.updateCSPPolicies(newRuleSet);
-            };
+    const updatePolicies = async () => {
+        const policies = {};
 
-            return <div key={domain} className={"cspmanager-block"}>
-                <TextInput value={domain} onChange={onChangeDomain} className="cspmanager-input" />
+        csPolicies.forEach(p => {
+            if (p.domain) {
+                policies[p.domain] = p.policy;
+            }
+        });
 
-                <Select
-                    options={[
-                        { value: ConnectSrc, label: "Connect" },
-                        { value: MediaSrc, label: "Media" },
-                        { value: CssSrc, label: "Css" },
-                        { value: MediaAndCssSrc, label: "Media&Css" },
-                        { value: MediaScriptsAndCssSrc, label: "Media&Scripts&Css" },
-                    ]}
-                    serialize={String}
-                    isSelected={v => v.join(" ") === policy}
-                    select={onChangePolicy}
-                    className={"cspmanager-select"}
-                />
-                <Button
-                    size={Button.Sizes.ICON}
-                    style={{ width: 44, height: 44, lineHeight: 0 }}
-                    color={Button.Colors.PRIMARY}
+        const successful = await Native.updateCSPPolicies(policies);
+
+        const key = openModal(modalProps => <ModalRoot {...modalProps}>
+            <ModalHeader>
+                <Text variant="heading-lg/semibold" style={{ flexGrow: 1 }}>CSPManager: Attention!</Text>
+                <ModalCloseButton onClick={() => closeModal(key)} />
+            </ModalHeader>
+
+            <ModalContent>
+                <Text variant="text-md/normal">
+                    {successful ? "Please reload your client for the changes to take effect." : "CSP modification cancelled."}
+                </Text>
+            </ModalContent>
+
+            <ModalFooter>
+                <Button color={Button.Colors.BRAND} onClick={() => window.location.reload()}>Reload Now</Button>
+            </ModalFooter>
+        </ModalRoot>);
+    };
+
+    function SettingsBlockRow({
+        id,
+        storedDomain,
+        storedPolicy,
+        onUpdate,
+        onDelete
+    }: {
+        id: string;
+        storedDomain:
+        string;
+        storedPolicy: string[];
+        onUpdate: (id: string, domain: string, policy: string[]) => void;
+        onDelete: (id: string) => void;
+    }) {
+        const [domain, setDomain] = useState(storedDomain);
+        // you cant directly compare arrays so this is the easiest solution lol
+        const [policy, setPolicy] = useState(storedPolicy.join(" "));
+
+        const onChangeDomain = (newDomain: string) => {
+            const newRuleSet = { ...csPolicies };
+            delete newRuleSet[domain];
+            newRuleSet[newDomain] = policy;
+
+            setDomain(newDomain);
+        };
+
+        const onLoseDomainFocus = () => {
+            onUpdate(id, domain, policy.split(" "));
+        };
+
+        const onChangePolicy = newPolicy => {
+            const newRuleSet = { ...csPolicies };
+            newRuleSet[domain] = newPolicy.value;
+
+            setPolicy(newPolicy.join(" "));
+            onUpdate(id, domain, newPolicy);
+        };
+
+        return <div className={"cspmanager-block"}>
+            <TextInput value={domain} onChange={onChangeDomain} onBlur={onLoseDomainFocus} className="cspmanager-input" />
+
+            <Select
+                options={[
+                    { value: ConnectSrc, label: "Connect" },
+                    { value: MediaSrc, label: "Media" },
+                    { value: CssSrc, label: "Css" },
+                    { value: MediaAndCssSrc, label: "Media&Css" },
+                    { value: MediaScriptsAndCssSrc, label: "Media&Scripts&Css" },
+                ]}
+                serialize={String}
+                isSelected={v => v.join(" ") === policy}
+                select={onChangePolicy}
+                className={"cspmanager-select"}
+            />
+
+            <Button
+                size={Button.Sizes.ICON}
+                style={{ width: 44, height: 44, lineHeight: 0 }}
+                color={Button.Colors.PRIMARY}
+                onClick={() => onDelete(id)}
+            >
+                <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    role="img"
                 >
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        role="img"
-                    >
-                        <path fill="var(--text-feedback-critical)" d="M17.3 18.7a1 1 0 0 0 1.4-1.4L13.42 12l5.3-5.3a1 1 0 0 0-1.42-1.4L12 10.58l-5.3-5.3a1 1 0 0 0-1.4 1.42L10.58 12l-5.3 5.3a1 1 0 1 0 1.42 1.4L12 13.42l5.3 5.3Z" />
-                    </svg>
-                </Button>
-            </div>;
-        })())}
+                    <path fill="var(--text-feedback-critical)" d="M17.3 18.7a1 1 0 0 0 1.4-1.4L13.42 12l5.3-5.3a1 1 0 0 0-1.42-1.4L12 10.58l-5.3-5.3a1 1 0 0 0-1.4 1.42L10.58 12l-5.3 5.3a1 1 0 1 0 1.42 1.4L12 13.42l5.3 5.3Z" />
+                </svg>
+            </Button>
+        </div>;
+    }
+
+    return <div className="cspmanager-container" ref={containerRef}>
+        {csPolicies.map(p => (
+            <SettingsBlockRow
+                key={p.id}
+                id={p.id}
+                storedDomain={p.domain}
+                storedPolicy={p.policy}
+                onDelete={onDelete}
+                onUpdate={onUpdate}
+            />
+        ))}
 
         <AddButton
             text={"Add Rule"}
             tooltip={"Add a CSP rule."}
-            onClick={() => { }}
+            className="cspmanager-add"
+            onClick={onAdd}
         />
+
+        <Button color={Button.Colors.BRAND} onClick={updatePolicies}>Save Content Security Policies</Button>
     </div>;
 }
 
--- native.ts
+++ native.ts
@@ -5,19 +5,60 @@
  */
 
 import { CspPolicies } from "@main/csp";
+import { NativeSettings } from "@main/settings";
+import { SettingsStore } from "@shared/SettingsStore";
+import { dialog } from "electron";
 
-export function getCurrentCSPPolicies(): typeof CspPolicies {
-    return { ...CspPolicies };
-}
+type NativeSettingsWithCSP = typeof NativeSettings & { cspPolicies: Record<string, string[]>; };
+const settings = NativeSettings as SettingsStore<Partial<NativeSettingsWithCSP>>;
 
-export function updateCSPPolicies(_event, policies: typeof CspPolicies) {
-    Object.entries(policies).forEach(([domain, policy]) => {
+if (settings.store.cspPolicies) {
+    Object.entries(settings.store.cspPolicies).forEach(([domain, policy]) => {
         CspPolicies[domain] = policy;
     });
 
     Object.entries(CspPolicies).forEach(([domain, _policy]) => {
-        if (!(domain in policies)) {
+        if (!(domain in (settings.store.cspPolicies || {}))) {
             delete CspPolicies[domain];
         }
+    });
+}
+
+export function getCurrentCSPPolicies(): typeof CspPolicies {
+    if (!settings.store.cspPolicies) {
+        settings.store.cspPolicies = { ...CspPolicies };
+    }
+
+    return settings.plain.cspPolicies || {};
+}
+
+export async function updateCSPPolicies(_event, policies: typeof CspPolicies): Promise<boolean> {
+    return await dialog.showMessageBox({
+        type: "question",
+        buttons: ["Yes", "No"],
+        defaultId: 0,
+        cancelId: 1,
+        title: "Modify CSP rules?",
+        message: `A plugin would like to modify the current CSP rules to:\n${Object.entries(policies).map(([domain, rules]) =>
+            `${domain}: ${rules.join(" ")}`
+        ).join("\n")}`,
+    }).then(res => {
+        if (res.response !== 0) {
+            return false;
+        }
+
+        settings.store.cspPolicies = policies;
+
+        Object.entries(policies).forEach(([domain, policy]) => {
+            CspPolicies[domain] = policy;
+        });
+
+        Object.entries(CspPolicies).forEach(([domain, _policy]) => {
+            if (!(domain in policies)) {
+                delete CspPolicies[domain];
+            }
+        });
+
+        return true;
     });
 }
